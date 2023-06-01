62a21b9ee8fcb3bfdf3521bf00dd61fbc686d5da
yuttana2002 <yuttanapanaree@gmail.com>
2023-06-01 21:36:11 +0700

    first commit

---
+++ src/views/demo/demo.js
@@ -0,0 +1,8 @@
+/* eslint-disable prettier/prettier */ 
+import React from 'react'
+
+export const demo = () => {
+  return (
+    <div>what ta fuck React</div>
+  )
+}
---
+++ src/views/employeeInfo/employeeInfo.js
@@ -0,0 +1,76 @@
+/* eslint-disable prettier/prettier */
+import React from 'react'
+import department from './department'
+import {
+  CCard,
+  CCardBody,
+  CCardHeader,
+  CCol,
+  CRow,
+  CAccordion,
+  CAccordionBody,
+  CAccordionHeader,
+  CAccordionItem,
+  CFormInput,
+} from '@coreui/react'
+import { DocsExample } from 'src/components'
+
+const employeeInfo = () => {
+  return (
+    <>
+      <CAccordion alwaysOpen activeItemKey={2}>
+        <CAccordionItem itemKey={1}>
+          <CAccordionHeader>พนักงานทั้งหมด</CAccordionHeader>
+
+          <CAccordionBody>
+            <CFormInput
+              type="text"
+              size="sm"
+              placeholder="Search.."
+              aria-label="sm input example"
+            />
+            <br />
+            <CAccordionItem itemKey={3}>
+              <CAccordionHeader>สาขา ไมด้าแอร์พอร์ต</CAccordionHeader>
+              <CAccordionBody>
+                <CAccordion activeItemKey={5}>
+                  <CAccordionItem itemKey={1}>
+                    <CAccordionHeader>ฝ่ายขาย</CAccordionHeader>
+                    <CAccordionBody>
+                      
+                    </CAccordionBody>
+                  </CAccordionItem>
+                  <CAccordionItem itemKey={2}>
+                    <CAccordionHeader>ทรัพยากรบุคคล</CAccordionHeader>
+                    <CAccordionBody>
+                  
+                    </CAccordionBody>
+                  </CAccordionItem>
+                  <CAccordionItem itemKey={3}>
+                    <CAccordionHeader>บัญชี</CAccordionHeader>
+                    <CAccordionBody>
+                  
+                    </CAccordionBody>
+                  </CAccordionItem>
+                  <CAccordionItem itemKey={4}>
+                    <CAccordionHeader>แม่บ้าน</CAccordionHeader>
+                    <CAccordionBody>
+                  
+                    </CAccordionBody>
+                  </CAccordionItem>
+                  
+                </CAccordion>
+              </CAccordionBody>
+            </CAccordionItem>
+          </CAccordionBody>
+        </CAccordionItem>
+        <CAccordionItem itemKey={2}>
+          <CAccordionHeader>พนักงานลาออก</CAccordionHeader>
+          <CAccordionBody></CAccordionBody>
+        </CAccordionItem>
+      </CAccordion>
+    </>
+  )
+}
+
+export default employeeInfo
